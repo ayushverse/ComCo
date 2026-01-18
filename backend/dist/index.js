9692ed45f272d06ee27412d5076b80d4484723e9
ayushverse <56singh5055@gmail.com>
2026-01-18 21:52:57 +0530

    backend for brodcasting or group chat is done

---
+++ backend/dist/index.js
@@ -0,0 +1,23 @@
+import { WebSocketServer, WebSocket } from 'ws';
+const wss = new WebSocketServer({ port: 8080 });
+let userCount = 0;
+let clientStorage = [];
+wss.on('connection', (socket) => {
+    clientStorage.push(socket);
+    userCount++;
+    console.log(`user # ${userCount} connected`);
+    socket.on('message', (message) => {
+        console.log(`received: ${message}`);
+        for (let i = 0; i < clientStorage.length; i++) {
+            const s = clientStorage[i];
+            //@ts-ignore
+            s.send(message.toString());
+        }
+    });
+    socket.on('close', () => {
+        clientStorage = clientStorage.filter(s => s !== socket);
+        userCount--;
+        console.log(`user disconnected. current user count: ${userCount}`);
+    });
+});
+//# sourceMappingURL=index.js.map
