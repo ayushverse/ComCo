79d81cb70b6d5d1abb6508023193f352bc9aae6d
ayushverse <56singh5055@gmail.com>
2026-01-20 20:16:07 +0530

    backend for room chat done

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,21 +1,51 @@
-import { WebSocketServer, WebSocket } from 'ws';
+import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 let userCount = 0;
 let clientStorage = [];
-wss.on('connection', (socket) => {
-    clientStorage.push(socket);
+wss.on("connection", (socket) => {
     userCount++;
-    console.log(`user # ${userCount} connected`);
-    socket.on('message', (message) => {
-        console.log(`received: ${message}`);
-        for (let i = 0; i < clientStorage.length; i++) {
-            const s = clientStorage[i];
-            //@ts-ignore
-            s.send(message.toString());
+    console.log(`user #${userCount} connected`);
+    socket.on("message", (message) => {
+        const parsedMessage = JSON.parse(message.toString());
+        //join room
+        if (parsedMessage.type === "join") {
+            console.log(`user joined room: ${parsedMessage.payload.roomId}`);
+            clientStorage.push({
+                socket,
+                room: parsedMessage.payload.roomId,
+            });
+        }
+        // chat
+        if (parsedMessage.type === "chat") {
+            let currentUserRoom = null;
+            for (let i = 0; i < clientStorage.length; i++) {
+                //find current user's room
+                //@ts-ignore
+                if (clientStorage[i].socket === socket) {
+                    //@ts-ignore
+                    currentUserRoom = clientStorage[i].room;
+                    break;
+                }
+            }
+            if (!currentUserRoom)
+                return;
+            //message broadcast to the room
+            for (let i = 0; i < clientStorage.length; i++) {
+                //@ts-ignore
+                if (clientStorage[i].room === currentUserRoom) {
+                    //@ts-ignore
+                    clientStorage[i].socket.send(JSON.stringify({
+                        type: "chat",
+                        payload: {
+                            message: parsedMessage.payload.message,
+                        },
+                    }));
+                }
+            }
         }
     });
-    socket.on('close', () => {
-        clientStorage = clientStorage.filter(s => s !== socket);
+    socket.on("close", () => {
+        clientStorage = clientStorage.filter((client) => client.socket !== socket);
         userCount--;
         console.log(`user disconnected. current user count: ${userCount}`);
     });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,72 @@
-import {WebSocketServer ,WebSocket} from 'ws'
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({port:8080})
-let userCount = 0
-let clientStorage : WebSocket[] = []
+type Client = {
+    socket: WebSocket;
+    room: string;
+};
 
-wss.on('connection' , (socket) => {
+const wss = new WebSocketServer({ port: 8080 });
 
-    clientStorage.push(socket)
-    userCount++
-    console.log(`user # ${userCount} connected`)
+let userCount = 0;
+let clientStorage: Client[] = [];
 
-    socket.on('message' , (message) => {
-        console.log(`received: ${message}`)
+wss.on("connection", (socket) => {
+    userCount++;
+    console.log(`user #${userCount} connected`);
 
-        for(let i =0;i<clientStorage.length;i++){
-            const s = clientStorage[i];
-            //@ts-ignore
-            s.send(message.toString())
+    socket.on("message", (message) => {
+        const parsedMessage = JSON.parse(message.toString());
+
+        //join room
+        if (parsedMessage.type === "join") {
+            console.log(`user joined room: ${parsedMessage.payload.roomId}`);
+
+            clientStorage.push({
+                socket,
+                room: parsedMessage.payload.roomId,
+            });
         }
-    })
 
-    socket.on('close' , () => {
-        clientStorage = clientStorage.filter(s => s !== socket)
-        userCount--
-        console.log(`user disconnected. current user count: ${userCount}`)
-    })
-})
+        // chat
+        if (parsedMessage.type === "chat") {
+            let currentUserRoom: string | null = null;
+
+            for (let i = 0; i < clientStorage.length; i++) {
+                //find current user's room
+                //@ts-ignore
+                if (clientStorage[i].socket === socket) {
+                    //@ts-ignore
+                    currentUserRoom = clientStorage[i].room;
+                    break;
+                }
+            }
+
+            if (!currentUserRoom) return;
+
+            //message broadcast to the room
+            for (let i = 0; i < clientStorage.length; i++) {
+                //@ts-ignore
+                if (clientStorage[i].room === currentUserRoom) {
+                    //@ts-ignore
+                    clientStorage[i].socket.send(
+                        JSON.stringify({
+                            type: "chat",
+                            payload: {
+                                message: parsedMessage.payload.message,
+                            },
+                        })
+                    );
+                }
+            }
+        }
+    });
+
+    socket.on("close", () => {
+        clientStorage = clientStorage.filter(
+            (client) => client.socket !== socket
+        );
+        userCount--;
+
+        console.log(`user disconnected. current user count: ${userCount}`);
+    });
+});
