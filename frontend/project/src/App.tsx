b4e78e6668fe2ed6c4d1bbfa5fd8842aa898c3b0
ayushverse <56singh5055@gmail.com>
2026-01-20 20:38:03 +0530

    started frontend will make it pretty later first trying to connect with websocket server

--- frontend/project/src/App.tsx
+++ frontend/project/src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
     return (
-        <div>
-
+        <div className="bg-black h-screen flex flex-col">
+            <div className="h-96/100 bg-red-600"> </div>
+            <div className="bg-white w-full flex flex-col">
+                <input type="text" className="w-full p-4"></input>
+                <button className="bg-purple-600 w-full">Send Message</button>
+            </div>
         </div>
     )
 }
