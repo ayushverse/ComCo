55613d32b5a97d52ed9b4d86e25891c595faf042
ayushverse <56singh5055@gmail.com>
2026-01-20 20:59:25 +0530

    started frontend will make it pretty later first trying to connect with websocket server

--- frontend/project/src/App.tsx
+++ frontend/project/src/App.tsx
@@ -1,13 +1,23 @@
-import { useEffect , useState } from "react";
+import { useEffect , useState , useRef} from "react";
 
 function App() {
-    const [messages, setMessages] = useState(["hi there" , "hi" , "hoooo"]);
+    const [messages, setMessages] = useState(["hi there", "hi", "hoooo"])//initial messages
+    const wsRef = useRef()
     useEffect(() => {
-        const ws = new WebSocket('ws://localhost:8080/ws')
+        const ws = new WebSocket('ws://localhost:8080/ws') //connect to server
         ws.onmessage = (event) => {
-            setMessages(m => [...m ,event.data])
+            setMessages(m => [...m ,event.data]) //eg- hi there , hi , hoooo , new message
         }
-    }, []) //empty array : when mounted websocket connects
+        wsRef.current = ws
+        ws.onopen = () => {
+            ws.send(JSON.stringify({
+                type: "join",
+                payload: {
+                    roomId: "room1"
+                }
+            }))
+        }
+    }, []) //receive messages , empty array : when mounted websocket connects
     return (
         <div className="bg-black h-screen flex flex-col">
             <br/> <br/> <br/>
@@ -15,8 +25,17 @@ function App() {
                 {messages.map(message =><div className="mt-10"> <span className = "bg-white text-black rounded p-4 m-8">{message}</span> </div>)}
             </div>
             <div className="bg-white w-full flex flex-col">
-                <input type="text" className="w-full p-4"></input>
-                <button className="bg-purple-600 w-full">Send Message</button>
+                <input id="message" type="text" className="w-full p-4"></input>
+                <button className="bg-purple-600 w-full" onClick={() => {
+                    //@ts-ignore
+                    const message = document.getElementById("message").value
+                    wsRef.current.send(JSON.stringify({
+                        type: "chat",
+                        payload: {
+                            message: message,
+                        },
+                    }))
+                }}>Send Message</button>
             </div>
         </div>
     )
