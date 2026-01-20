a3204d8e1d03ae91391c37a1b21978965eafd970
ayushverse <56singh5055@gmail.com>
2026-01-20 20:49:15 +0530

    started frontend will make it pretty later first trying to connect with websocket server

--- frontend/project/src/App.tsx
+++ frontend/project/src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect , useState } from "react";
+
 function App() {
+    const [messages, setMessages] = useState(["hi there" , "hi" , "hoooo"]);
+    useEffect(() => {
+        const ws = new WebSocket('ws://localhost:8080/ws')
+        ws.onmessage = (event) => {
+            setMessages(m => [...m ,event.data])
+        }
+    }, []) //empty array : when mounted websocket connects
     return (
         <div className="bg-black h-screen flex flex-col">
-            <div className="h-96/100 bg-red-600"> </div>
+            <br/> <br/> <br/>
+            <div className="h-96/100 bg-black">
+                {messages.map(message =><div className="mt-10"> <span className = "bg-white text-black rounded p-4 m-8">{message}</span> </div>)}
+            </div>
             <div className="bg-white w-full flex flex-col">
                 <input type="text" className="w-full p-4"></input>
                 <button className="bg-purple-600 w-full">Send Message</button>
